441f5006441d6ab123bca02ddc57c2094bffef8b
Michelon Souza <michelon.souza@hotmail.com>
2020-08-03 22:14:13 -0300

    add pageHeader component

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,10 @@ import React from 'react';
 
 import './assets/styles/global.css';
 
-import Landing from './pages/Landing';
+import Routes from './routes';
 
 const App: React.FC = () => {
-  return <Landing />;
+  return <Routes />;
 };
 
 export default App;
---
+++ src/pages/index.ts
@@ -0,0 +1,3 @@
+export { default as Landing } from './Landing';
+export { default as TeacherForm } from './TeacherForm';
+export { default as TeacherList } from './TeacherList';
